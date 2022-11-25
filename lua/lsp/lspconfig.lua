52f39222c691ecbbc101ce6f37c98c1964223cf9
Gabriele Stulzer <gstulzer@fbk.eu>
2022-11-25 12:24:58 +0100

    Add relnumbers and dart

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -127,7 +127,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches.
 -- Add your language server below:
-local servers = { 'bashls', 'cmake', 'pyright', 'gopls', 'html', 'cssls', 'texlab', 'tsserver', 'tailwindcss', 'sourcekit' }
+local servers = { 'bashls', 'cmake', 'dartls', 'pyright', 'gopls', 'html', 'cssls', 'texlab', 'tsserver', 'tailwindcss', 'sourcekit' }
 
 -- Call setup
 for _, lsp in ipairs(servers) do
