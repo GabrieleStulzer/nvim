7982b157b28ec6d7e645fda2c0eb40e1448529bd
Gabriele Stulzer <gstulzer@fbk.eu>
2022-12-26 22:24:11 +0100

    Add configs and keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,8 +10,14 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
--- Change leader to a comma
+-- Change leader to space
 vim.g.mapleader = ','
+vim.g.maplocalleader = ','
+
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -----------------------------------------------------------
 -- Neovim shortcuts
@@ -43,6 +49,12 @@ map('n', '<C-j>', '<C-w>j')
 map('n', '<C-k>', '<C-w>k')
 map('n', '<C-l>', '<C-w>l')
 
+-- Resize Splits
+map('n', '<C-S-H>', ':vertical -1<CR>')
+map('n', '<C-S-J>', ':res +1<CR>')
+map('n', '<C-S-K>', ':res -1<CR>')
+map('n', '<C-S-L>', ':vertical +1<CR>')
+
 -- Reload configuration without restart nvim
 map('n', '<leader>r', ':so %<CR>')
 
@@ -53,13 +65,46 @@ map('i', '<leader>s', '<C-c>:w<CR>')
 -- Close all windows and exit from Neovim with <leader> and q
 map('n', '<leader>q', ':qa!<CR>')
 
+local move_down;
+local move_up;
+local move_down_shift;
+local move_up_shift;
+
+if vim.fn.has('macunix') then
+  move_down =  '∆'
+  move_up =  '˚'
+  move_down_shift = 'Ô'
+  move_up_shift = ''
+else
+  move_down = '<A-j>'
+  move_up =  '<A-k>'
+  move_down_shift = '<A-S-j>'
+  move_up_shift = '<A-S-k>'
+end
+
+-- Move Lines
+map('n', move_down, ':m .+1<CR>==')
+map('n', move_up, ':m .-2<CR>==')
+map('i', move_down, '<Esc>:m .+1<CR>==gi')
+map('i', move_up, '<Esc>:m .-2<CR>==gi')
+map('v', move_down, ':m \'>+1<CR>gv=gv')
+map('v', move_up, ':m \'<-2<CR>gv=gv')
+
+--- Move lines by 10 Rows at a time
+map('n', move_down_shift, ':m .+10<CR>==')
+map('n', move_up_shift, ':m .-11<CR>==')
+map('v', move_down_shift, ':m \'>+10<CR>gv=gv')
+map('v', move_up_shift, ':m \'<-11<CR>gv=gv')
+
 -----------------------------------------------------------
 -- Applications and Plugins shortcuts
 -----------------------------------------------------------
 
 -- Terminal mappings
 map('n', '<C-t>', ':Term<CR>', { noremap = true })  -- open
-map('t', '<Esc>', '<C-\\><C-n>')                    -- exit
+
+--- Do not use this binding if you are usign vi mode with zsh
+-- map('t', '<Esc>', '<C-\\><C-n>')                    -- exit
 
 -- NvimTree
 map('n', '<C-n>', ':NvimTreeToggle<CR>')            -- open/close
@@ -68,3 +113,20 @@ map('n', '<leader>n', ':NvimTreeFindFile<CR>')      -- search file
 
 -- Tagbar
 map('n', '<leader>z', ':TagbarToggle<CR>')          -- open/close
+
+-- Telescope
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer]' })
+
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
